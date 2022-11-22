cbf983073fe1ba279df30844ce18a7196e5979f6
Arslan Okenov <gtg100900@gmail.com>
2022-11-22 16:35:55 +0600

    added width for App

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Messages from "./Components/Messages/Messages";
 
 function App() {
   return (
-    <div className="App text-center m-auto">
+    <div className="App text-center m-auto" style={{width: '890px'}}>
       <Form/>
       <h1>Messages</h1>
       <Messages/>
